fa2da4b8abb001eb9a713701944b26ba3f6206fc
grigorii goncharov <grigoriy85@gmail.com>
2025-10-13 15:53:57 +0300

    Админ панель htracker

--- htracker/admin.py
+++ htracker/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from htracker.models import Habit
+
+
+@admin.register(Habit)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("owner", "place", "time", "action", "pleasant_habit", "related_habit")
+    search_fields = ("owner", "pleasant_habit")
+
+
+
