19640ec3d03e73349f7e91ff20be4dac136851ac
grigorii goncharov <grigoriy85@gmail.com>
2025-10-14 17:43:18 +0300

    Метод validate на проверку данных в HabitSerializer

--- htracker/serializers.py
+++ htracker/serializers.py
@@ -3,14 +3,16 @@ from .models import Habit
 
 
 class HabitSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели привычки"""
     class Meta:
         model = Habit
         fields = '__all__'
         read_only_fields = ('owner',)
 
     def validate(self, data):
-        # Повторная валидация на уровне сериализатора (на всякий случай)
-        habit = Habit(**data)
-        habit.clean()
+        # Валидация модели только при создании
+        if not self.instance:
+            temp = Habit(**data)
+            temp.clean()
         return data
 
