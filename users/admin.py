8310f2469d212fd9f3492317cb3b9aca005fbcd2
grigorii goncharov <grigoriy85@gmail.com>
2025-10-14 07:54:56 +0300

    поправлен вывод в Админку Юзера

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,10 @@
-from django.apps import AppConfig
+from django.contrib import admin
+
+from users.models import User
 
 
-class UsersConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "users"
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "phone", "telegram_chat_id")
+    search_fields = ("email", "phone")
+
