41176c2eda00f8f913e3576c877458be682e50d0
grigorii goncharov <grigoriy85@gmail.com>
2025-11-02 16:12:15 +0300

    Fix5 settings.py STATIC_ROOT = BASE_DIR / "staticfiles" добавлен STATIC_ROOT = BASE_DIR / "staticfiles"

--- config/settings.py
+++ config/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 
 
 STATIC_URL = "static/"
-
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"  # сюда collectstatic будет копировать всё
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
