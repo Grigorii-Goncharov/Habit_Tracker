0e5a68f864bea4f5d1605bcb2967a90d7fbb81bb
grigorii goncharov <grigoriy85@gmail.com>
2025-11-02 16:32:51 +0300

    DIX3 DEPLOY DOCKER

--- htracker/tests/test_celery.py
+++ htracker/tests/test_celery.py
@@ -88,7 +88,7 @@ class HabitCeleryTest(TestCase):
     ):
         """Тест: привычка не требует напоминания и не проваливается"""
 
-        Habit.objects.create(
+        habit = Habit.objects.create(   # noqa: F841 - игнорирование строки для flake8
             owner=self.user,
             place="Дом",
             time=time(10, 0),
